ac31f3607d77266b9fff92825ecb617c7aac8b6a
Alex4141 <agoncalves4141@aol.com>
2017-02-09 19:54:16 -0500

    Added client and server for file transfer program

---
+++ client.py
@@ -0,0 +1,39 @@
+import socket
+import os
+
+# Basic Configuration for socket, looks at port 8080
+def socketConf():
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.setblocking(1)
+	sock.connect(('',8080))
+	return sock
+
+# Creates a directory for file transfers
+def getDirectory():
+	relativePath = os.path.join(os.getcwd(),'forTransfers')
+	if not os.path.isdir(relativePath):
+		os.mkdir('forTransfers')
+	return relativePath
+
+# Creates a file
+def createFile(currentSocket):
+	currentDirectory = getDirectory()
+	filename = currentDirectory + '/' +  currentSocket.recv(1024)
+	newFile = open(filename, 'wb')
+	appendData(currentSocket, newFile)
+
+# Get 1024 bytes at a time and append it to the file
+def appendData(currentSocket, transferFile):
+	while True:
+		chunk = currentSocket.recv(1024)
+		if chunk:
+			transferFile.write(chunk)
+			chunk = currentSocket.recv(1024)
+		else:
+			transferFile.close()
+			print("FTP Complete")
+			break
+
+sock = socketConf()
+createFile(sock)
+sock.close()
---
+++ server.py
@@ -0,0 +1,39 @@
+import socket
+import os
+from sys import argv
+
+# Function takes the CLI argument and returns the absolute path
+def getPath():
+	if len(argv) > 1 and os.path.exists(argv[1]):
+		return open(argv[1], 'rb')
+
+# Basic Configuration for socket, binds at port 8080
+def socketConf():
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.bind(('',8080))
+	sock.listen(1)
+	return sock
+
+# Two states: Passing the filename and then passing file content
+def stateSend(connectionSocket, serveFile, currentState):
+	if currentState == 0:
+		connectionSocket.send(os.path.split(serveFile.name)[1])
+		stateSend(connectionSocket,serveFile, 1)
+	else:
+		for line in serveFile:
+			connectionSocket.send(line)
+		connectionSocket.close()
+
+'''
+Get the file and create socket
+Wait for connections
+Send file content
+'''
+currFile = getPath()
+sock = socketConf()
+
+while True:
+	conn, address = sock.accept()
+	stateSend(conn,currFile,0)
+	conn.close()
+	break
